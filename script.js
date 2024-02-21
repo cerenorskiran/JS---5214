0262167b18e0c86e6ba5b292d2a5ccee72ee850c
Ceren Örskıran <55747316+cerenorskiran@users.noreply.github.com>
2024-02-21 21:58:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.className === "navbar") {
+      navbar.className += " responsive";
+    } else {
+      navbar.className = "navbar";
+    }
+  }
+
